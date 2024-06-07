12b2bc14f68233d02ebeabef3bd73506b8c09b0d
Johanan <117050032+Johanan-Dream@users.noreply.github.com>
2024-06-07 13:42:43 +0900

    [Bronze I] Title: 단어 공부, Time: 632 ms, Memory: 49648 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,31 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine().toUpperCase();
+
+        char[] arr = text.toCharArray();
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(char c : arr){
+            map.put(c, map.getOrDefault(c, 0)+1);
+        }
+
+        Integer maxValue = Collections.max(map.values());
+        List<String> result = map.entrySet()
+            .stream()
+            .filter(entry -> entry.getValue().equals(maxValue))
+            .map(entry -> entry.getKey().toString())
+            .collect(Collectors.toList());
+
+        if(result.size() >= 2){
+            System.out.println("?");
+        }else if(result.size() == 1){
+            System.out.println(result.get(0).toUpperCase());
+        }
+    }
+}
