61a92c40587ab6fd92ac844b9bce07a4c5c4a1db
Johanan <117050032+Johanan-Dream@users.noreply.github.com>
2024-06-13 10:20:03 +0900

    Update 대소문자 바꿔서 출력하기.java

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -15,6 +15,9 @@ public class Solution {
             }
         }
         String result = new String(array_word);
+        //char 배열을 String으로 변환하는 방법 
+        //1. String 생성자 2. String.valueOf() 3. StringBuilder
+        //1,2 간단하나 큰 배열의 경우 메모리 사용량이 증가할 수 있음 3. 메모리 효율성이 높지만 코드가 상대적으로 복잡해질 수 있음.
         System.out.println(result);
     }
 }
