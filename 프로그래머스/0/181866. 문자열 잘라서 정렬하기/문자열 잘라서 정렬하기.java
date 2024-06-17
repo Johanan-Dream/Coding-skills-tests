b37eb175d4b7609d38d6fd7bbfc556fa9c65cc30
Johanan <117050032+Johanan-Dream@users.noreply.github.com>
2024-06-17 15:47:41 +0900

    Update 문자열 잘라서 정렬하기.java

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -29,3 +29,33 @@ class Solution {
     }
 }
 
+//배열 -> 리스트, 리스트 -> 배열
+//테스트 1 〉	통과 (25.86ms, 96.5MB)
+//테스트 2 〉	통과 (21.79ms, 88.9MB)
+//테스트 3 〉	통과 (9.19ms, 80.4MB)
+//테스트 4 〉	통과 (6.18ms, 75.5MB)
+//테스트 5 〉	통과 (17.29ms, 84.8MB)
+//테스트 6 〉	통과 (11.49ms, 74.3MB)
+//테스트 7 〉	통과 (13.22ms, 89.3MB)
+//테스트 8 〉	통과 (25.32ms, 99.8MB)
+//테스트 9 〉	통과 (15.72ms, 88.6MB)
+//테스트 10 〉	통과 (24.34ms, 86.9MB)
+//테스트 11 〉	통과 (15.64ms, 92.2MB)
+import java.util.*;
+class Solution {
+    public String[] solution(String myString) {
+        String[] answer = {};
+        //1. split하기 
+        answer = myString.split("x");
+        //2. list로 변환하기 
+        List<String> strList = new ArrayList<String>(Arrays.asList(answer));
+        //3. 공백 제거 & 정렬
+        strList.removeAll(Arrays.asList("", null));
+        Collections.sort(strList);
+        
+        //4. 다시 배열로 변환
+        answer = strList.toArray(String[]::new);
+        return answer;
+    }
+}
+
