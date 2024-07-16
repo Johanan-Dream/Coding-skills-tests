b395507140968fa7cff71726d8673914905bf747
Johanan <117050032+Johanan-Dream@users.noreply.github.com>
2024-07-16 14:22:34 +0900

    [level 1] Title: K번째수, Time: 0.55 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,25 +1,11 @@
 import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        		int[] answer = new int[commands.length];
-
-		int[] test = {};
+        int[] answer = new int[commands.length];
 		for(int i = 0; i < commands.length; i++){
-			int start = commands[i][0];
-			int end = commands[i][1];
-			int choice = commands[i][2];
-
-			test = new int[end - start + 1];
-			int x = 0;
-			for(int j = start; j <= end; j++){
-				//꺼내기
-				test[x] = array[j-1];
-				x++;
-			}
-			//정렬하기
-			test = Arrays.stream(test).sorted().toArray();
-			int a = test[choice-1];
-			answer[i] = a;
+			int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+			Arrays.sort(temp);
+			answer[i] = temp[commands[i][2]-1];
 		}
 		return answer;
     }
