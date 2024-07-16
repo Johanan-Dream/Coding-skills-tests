2999f6782d891052dbf30a31573fc4f488f1791e
Johanan <117050032+Johanan-Dream@users.noreply.github.com>
2024-07-16 14:24:17 +0900

    Update K번째수.java

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,3 +1,37 @@
+
+//방법1
+import java.util.Arrays;
+class Solution {
+	public static int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		
+		int[] test = {};// 임시로 값을 담아둘 배열
+
+		//1번 반복문
+		for(int i = 0; i < commands.length; i++){
+			int start = commands[i][0];
+			int end = commands[i][1];
+			int choice = commands[i][2];
+
+			test = new int[end - start + 1];
+			
+			//2번 반복문
+			int x = 0;
+			for(int j = start; j <= end; j++){
+				//꺼내기
+				test[x] = array[j-1];
+				x++;
+			}
+			//정렬하기
+			test = Arrays.stream(test).sorted().toArray();
+			int a = test[choice-1];
+			answer[i] = a;
+		}
+		return answer;
+	}
+}
+
+//방법2
 import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
@@ -10,3 +44,5 @@ class Solution {
 		return answer;
     }
 }
+
+//방법2가 더 빠름
